65459525bd8a076a6c704657e10a1c31ee397068
Taha <Taha@Ezgis-Mac-mini.local>
2021-03-30 12:19:16 +0300

    Requests are completed

--- Pods/Target Support Files/Pods-CurrencyApp-CurrencyAppUITests/Pods-CurrencyApp-CurrencyAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CurrencyApp_CurrencyAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CurrencyApp_CurrencyAppUITests
-@end
--- Pods/Target Support Files/Pods-CurrencyAppTests/Pods-CurrencyAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CurrencyAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_CurrencyAppTests
-@end
